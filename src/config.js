c2145b373d430927f6fcea4ba1f4ebb0581665ba
Caian R. Ertl <hi@caian.org>
2019-03-26 19:30:02 -0300

    feat(config): return null if anything goes wrong

--- src/config.js
+++ src/config.js
@@ -5,8 +5,15 @@ module.exports = () =>
     const name = process.env.CONFIG_FILEPATH || 'config.yml';
     const file = new File(name);
 
-    if (!file.isReady())
-        return null;
+    return (() => {
+        try {
+            if (!file.isReady())
+                return null;
 
-    return file.toYAML();
+            return file.toYAML();
+        }
+        catch (e) {
+            return null;
+        }
+    })();
 }
