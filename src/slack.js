87d7e4d233b9d870847798f86f54f6906865c713
Caian R. Ertl <hi@caian.org>
2019-04-03 22:03:37 -0300

    feat(slack): implement the POST request to Slack

--- src/slack.js
+++ src/slack.js
@@ -1,7 +1,44 @@
+const https = require('https');
+
+
+/**
+    --- TODO: docs ---
+ */
 function notify(channel, message)
 {
+    const info = {
+        hostname: 'hooks.slack.com',
+        path:     `/services/${channel}`,
+        method:   'POST',
+        port:     443,
+        headers:  {
+            'Content-Type':  'application/json',
+            'Content-Length': message.length,
+        }
+    };
+
+    const success = (() => {
+        const req = https.request(info, (res) => {
+            console.log(res.statusCode);
+            return res.statusCode === 200;
+        });
+
+        req.on('error', (e) => {
+            console.log(e);
+            return false;
+        });
+
+        req.write(message);
+        req.end();
+    })();
+
+    return success;
 }
 
+
+/**
+    --- TODO: docs ---
+ */
 module.exports = {
     notify: notify
 }
