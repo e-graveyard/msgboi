0b7e8e4e5f94013a9677877103f1b8d9b02091bc
Caian R. Ertl <caian.ertl@rivendel.com.br>
2019-04-06 03:46:53 -0300

    refact(error): group the event information inside a hash

--- src/msgboi/error.js
+++ src/msgboi/error.js
@@ -6,7 +6,9 @@ module.exports = class MsgboiError extends Error
     constructor(code, message) {
         super();
 
-        this.code = code;
-        this.message = message;
+        this.content = {
+            code: code,
+            message: message,
+        };
     }
 };
