c2ab2bd6f9ca4519b4474cc5a733d7cff64b7219
Caian R. Ertl <hi@caian.org>
2019-04-08 23:33:59 -0300

    feat(error): MsgboiException can contain the parent error object

--- src/msgboi/error.js
+++ src/msgboi/error.js
@@ -3,11 +3,12 @@
  */
 module.exports = class MsgboiError extends Error
 {
-    constructor(code, message) {
+    constructor(code, message, parentError = undefined) {
         super();
 
         this.content = {
             code: code,
+            error: parentError,
             message: message,
         };
     }
