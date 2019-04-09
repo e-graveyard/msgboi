73f55616b8d10d12552eafcf18797eb09f45e525
Caian R. Ertl <hi@caian.org>
2019-04-09 00:31:23 -0300

    refact(config): simplify config loading

--- src/msgboi/config.js
+++ src/msgboi/config.js
@@ -6,20 +6,31 @@ const file = require('./file');
  */
 function load()
 {
+    /*
+    msgboi reads the "MSGBOI_CONFIG" environment variable expecting a filename.
+    If the variable is set, it tries to read the file. If it's not, it
+    fallbacks to the default config filename "config.yml".
+
+    The configuration file is a YAML-formatted document. If something goes
+    wrong reading and/or parsing the file, a null value will be returned,
+    throwing a MsgboiError exception.
+    */
     const name = process.env.MSGBOI_CONFIG || 'config.yml';
 
-    return (() => {
-        try {
-            if (!file.isReady(name))
-                return null;
+    if (!file.isReady(name))
+        throw new MsgboiError(500, 'unable to locate the config file');
 
-            return file.fromYAML(name);
-        }
-        catch (e) {
-            console.log(e);
-            return null;
-        }
-    })();
+    const c = file.fromYAML(name);
+    if (!c)
+        throw new MsgboiError(500, 'unable to load the configurations');
+
+    if (!(c.event && c.notification))
+        throw new MsgboiError(500, 'malformed configurations');
+
+    return {
+        event: c.event,
+        notification: c.notification,
+    };
 }
 
 
