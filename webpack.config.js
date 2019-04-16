b62e4144857192589b8bd1f314509b8dfe0b9bb1
Caian R. Ertl <caian.ertl@rivendel.com.br>
2019-04-15 23:36:13 -0300

    chore(webpack): exclude comments when minifying

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
     entry: './bundle/index.js',
     target: 'node',
+    optimization: {
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                output: { comments: false },
+            },
+        })],
+    },
     output: {
         filename: 'index.js',
         library: 'handler',
