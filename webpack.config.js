a40bde0284ca4f128006069295292da93d950bb4
Caian R. Ertl <hi@caian.org>
2021-04-21 18:26:08 -0300

    chore: output of webpack must be a commonjs module

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,10 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: 'handler',
+    libraryTarget: 'commonjs',
+    libraryExport: 'handler'
   },
   resolve: {
     extensions: ['.js']
