98f994f8f6394955ad9988d504f7f0269d82d254
Caian R. Ertl <hi@caian.org>
2021-04-21 18:10:20 -0300

    chore: reconfigure webpack to use babel and to generate minimum bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
+const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   mode: 'production',
-  entry: './bundle/index.js',
+  entry: path.resolve(__dirname, 'bundle', 'index.js'),
   target: 'node',
   optimization: {
+    minimize: true,
     minimizer: [
       new TerserPlugin({
+        extractComments: false,
         terserOptions: {
-          output: { comments: false }
+          output: {
+            comments: false
+          }
         }
       })
-    ]
+    ],
+    mangleExports: 'size',
+    moduleIds: 'size',
+    chunkIds: 'size'
   },
   output: {
-    filename: 'index.js',
-    library: 'handler',
-    libraryTarget: 'commonjs',
-    libraryExport: 'handler'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: 'babel-loader',
+        exclude: /node-modules/
+      }
+    ]
   }
 }
